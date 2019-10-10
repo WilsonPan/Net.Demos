504c42a9aca26ae09e9a1d08cb4d9a059fd99c1b
Wilson <panweipei_2010@163.com>
2019-10-10 11:57:30 +0800

    Initial commit

---
+++ Demo.AutoMapper/Program.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.AddMemberConfiguration();
+
+                cfg.CreateMap<Dto, Entity>()
+                   .ForMember(dest => dest.CreateTime, opt => opt.MapFrom(src => new DateTime(DateTime.Now.Ticks)));
+            });
+            try
+            {
+                var entity = Mapper.Map<Dto, Entity>(new Dto { Id = 1, Name = "Wilson", Hour = 12, Minute = 12, Second = 12 });
+
+                Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(entity));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey(true);
+        }
+    }
+
+
+    class Entity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+
+    public class Dto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+}
---
+++ Demo.RabbitMQ/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.UserName = "dev";
+            factory.Password = "dev";
+            factory.VirtualHost = "dev";
+            factory.HostName = "192.168.6.86";
+            factory.Port = 5672;
+
+            IConnection conn = factory.CreateConnection();
+
+            var model = conn.CreateModel();
+
+            byte[] messageBodyBytes = System.Text.Encoding.UTF8.GetBytes("Hello, world! " + DateTime.Now);
+            model.BasicPublish("", "Wilson", null, messageBodyBytes);
+
+            conn.Dispose();
+            model.Dispose();
+
+            Console.WriteLine("Finish");
+
+            Console.Read();
+        }
+    }
+}
